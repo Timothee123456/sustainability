fa16f7221d33e1d6f7f4651c8920ed692dd1a5d4
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-12-15 02:27:36 +0000

    add functionallity to remove ingredient

--- rating/src/App.js
+++ rating/src/App.js
@@ -1,41 +1,73 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import getRandomBrightColor from './utils/randomColor.js';
-import BasicRating from './components/old_rating/rating.tsx';
 import Ingredient from './components/ingredient.jsx';
 
 function changeBgColor(){
   document.body.style.backgroundColor = getRandomBrightColor() // Change background color to a random bright color
 }
 
+function reset(){
+  changeBgColor();
+}
+
 function App(date = '12-03-2025') {
-  const baseWidth = 300; // reference width for scaling at which rsize = 1
-  const [rsize, setRsize] = useState(window.innerWidth / baseWidth); // Initialize rsize based on initial width
+  const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
-    const ratingSize = () => {
-      setRsize(window.innerWidth / baseWidth);
-    };
-    window.addEventListener('resize', ratingSize);  // this will adjust the rating size based on window resize
-
+    fetchData();
     changeBgColor()
   }, []); // Empty dependency array: run only once on mount
 
-  function spacing(value){
-    return { marginBottom: value * rsize }
-  }
-  
+  const fetchData = async () => {
+      try {
+          const response = [
+            {
+              'name': 'Pork',
+              'type': 'Dish A',
+              'img_link': 'https://www.lovefoodhatewaste.com/sites/default/files/styles/16_9_two_column/public/2022-08/Pork-sh1419942758.jpg.webp?itok=_Ow0IXe6'
+            },
+            {
+              'name': 'Eggplant',
+              'type': 'Dish B',
+              'img_link': 'https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/eggplant.jpg.webp?itok=k6_74IzB'
+            },
+            {
+              'name': 'Cabbage',
+              'type': 'Vegetables',
+              'img_link': 'https://greengarden.ph/cdn/shop/products/LINE_ALBUM_PICTURE_230412_85.jpg?v=1681290016'
+            },
+            {
+              'name': 'Rice',
+              'type': 'Starch',
+              'img_link': 'https://healthynibblesandbits.com/wp-content/uploads/2018/10/Jasmine-Rice-FF.jpg'
+            },
+            {
+              'name': 'Sponge cake',
+              'type': 'Dessert',
+              'img_link': 'https://www.laserbiscuit.com/wp-content/uploads/2021/03/sponge-cake-stuck.jpg'
+            }
+          ];
+          setIngredients(response);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+  };
+
   return (
-    <div  className="container">
+    <div className="container">
       <h1>What ingredients did you like today?</h1>
       <div class="food-grid">
-        <Ingredient name="Pork" type="Dish A" img_link="https://www.lovefoodhatewaste.com/sites/default/files/styles/16_9_two_column/public/2022-08/Pork-sh1419942758.jpg.webp?itok=_Ow0IXe6" />
-        <Ingredient name="Eggplant" type="Dish B" img_link="https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/eggplant.jpg.webp?itok=k6_74IzB" />
-        <Ingredient name="Cabbage" type="Vegetables" img_link="https://greengarden.ph/cdn/shop/products/LINE_ALBUM_PICTURE_230412_85.jpg?v=1681290016" />
-        <Ingredient name="Rice" type="Starch" img_link="https://healthynibblesandbits.com/wp-content/uploads/2018/10/Jasmine-Rice-FF.jpg" />
-        <Ingredient name="Sponge cake" type="Dessert" img_link="https://www.laserbiscuit.com/wp-content/uploads/2021/03/sponge-cake-stuck.jpg" />
+        {ingredients.map((ingredient, index) => (
+          <Ingredient
+            key={index}
+            name={ingredient.name}
+            type={ingredient.type}
+            img_link={ingredient.img_link}
+          />
+        ))}
       </div>
-      <a className="myButton" onClick={changeBgColor}>
+      <a className="submit" onClick={reset}>
         Submit
       </a>
     </div>
--- rating/src/components/ingredient.jsx
+++ rating/src/components/ingredient.jsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 
 export default function Ingredient({ name, type, img_link }) {
     const [color, setColor] = useState(false);
+    const [isRemoved, setIsRemoved] = useState(false);
     
 
     const handleClick = () => {
+        if (isRemoved) return; // Do nothing if removed
         !color ? setColor('green') : color === 'green' ? setColor('red') : setColor(false);
     };
 
-    const className = `food-item ${color ? color : ''}`;
+    const handleRemoveClick = (e) => {
+        e.stopPropagation(); // Prevent triggering the parent onClick
+        setIsRemoved(!isRemoved);
+        setColor(false); // Reset color when removed
+    }
+
+    const className = `food-item ${color ? color : ''} ${isRemoved ? 'darkened' : ''}`;
+    const checkBoxName = isRemoved ? 'check_box_outline_blank' : 'check_box';
     
     return (
         <div 
@@ -17,6 +26,8 @@ export default function Ingredient({ name, type, img_link }) {
             data-type={type}
             onClick={handleClick}
         >
+            {/* <i class="material-icons remove-button" onClick={handleRemoveClick}>check</i> */}
+            <i class="material-icons remove-button" onClick={handleRemoveClick}>{checkBoxName}</i>
             <img
                 src={img_link}
                 className="food-image"
